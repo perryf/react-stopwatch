32ff83298b8b77fbdf5d85b799b3918d61c7d1ee
Perry Fustero <pjfust@gmail.com>
2017-09-06 10:44:15 -0400

    first commit, installed files, start button works on click, but no time element yet

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor() {
+    super()
+    this.state = {
+      counter: 0
+    }
+  }
+
+  startTimer(e) {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.counter}</h1>
         <div className="controls">
           <button>Reset</button>
-          <button>Start</button>
+          <button onClick={(e) => this.startTimer(e)}>Start</button>
           <button>Pause</button>
         </div>
       </div>
